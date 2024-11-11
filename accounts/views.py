45e8b704fe1c04c9383ef4f5bda76316b509b377
CoolnessMario <144562752+CoolnessMario@users.noreply.github.com>
2024-11-11 14:06:49 -0500

    Added account deletion

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib import messages
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+@login_required
+def delete_account(request):
+    if request.method == 'POST':
+        user = request.user
+        logout(request)
+        user.delete()
+        messages.success(request, 'Your account has been deleted.')
+        return redirect('home')
+    return render(request, 'delete_account.html')
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
