e8bef7da23a4c52a9053edf6e271f7c062561218
CoolnessMario <144562752+CoolnessMario@users.noreply.github.com>
2024-11-12 15:07:41 -0500

    Removed duplicate home file

--- spotify_wrapper/urls.py
+++ spotify_wrapper/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home/home.html'), name='home'),
     path('delete-account/', views.delete_account, name='delete_account'),
     path('accounts/', include('accounts.urls')),
     path('', include('home.urls')),
