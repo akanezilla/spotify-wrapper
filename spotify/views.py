d347bd2f07a0aed72e5943d2a9785f2a5704a8d0
akanezilla <sheim6@gatech.edu>
2024-11-19 14:15:27 -0500

    .idea and views changes

--- spotify/views.py
+++ spotify/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+gitfrom django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from spotipy.oauth2 import SpotifyOAuth
 from django.conf import settings
