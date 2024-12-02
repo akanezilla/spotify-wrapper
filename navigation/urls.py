ea7535e403d74489b62eefdc9b9567730f29f6b0
nli21 <norahjadeli@gmail.com>
2024-12-02 01:46:50 -0500

    light mode

--- navigation/urls.py
+++ navigation/urls.py
@@ -2,16 +2,26 @@ from django.urls import path
 from . import views
 from django.views.generic.base import RedirectView
 
-
 urlpatterns = [
     path('RainbowMode/transition1/', views.transition1_rb, name='transition1RB'),
     path('RainbowMode/transition2/', views.transition2_rb, name='transition2RB'),
     path('RainbowMode/slideShowRB/', views.slideshow_rb, name='slideShowRB'),
     path('RainbowMode/top_5_songs/', views.top_5_songs_rb, name='top_5_songsRB'),
     path('RainbowMode/top_song/', views.top_song_rb, name='top_songRB'),
-path('RainbowMode/top_artists/', views.top_artist_rb, name='top_artistsRB'),
+    path('RainbowMode/top_artists/', views.top_artist_rb, name='top_artistsRB'),
     path('RainbowMode/top_genre/', views.top_genre_rb, name='top_genreRB'),
     path('RainbowMode/listening_minutes/', views.listening_minutes_rb, name='listening_minutesRB'),
     path('RainbowMode/type_of_listener/', views.type_of_listener_rb, name='type_of_listenerRB'),
     path('RainbowMode/memorable_moment/', views.memorable_moment_rb, name='memorable_momentRB'),
+
+    path('LightMode/transition1/', views.transition1_lm, name='transition1LM'),
+    path('LightMode/transition2/', views.transition2_lm, name='transition2LM'),
+    path('LightMode/slideShowRB/', views.slideshow_lm, name='slideShowLM'),
+    path('LightMode/top_5_songs/', views.top_5_songs_lm, name='top_5_songsLM'),
+    path('LightMode/top_song/', views.top_song_lm, name='top_songLM'),
+    path('LightMode/top_artists/', views.top_artist_lm, name='top_artistsLM'),
+    path('LightMode/top_genre/', views.top_genre_lm, name='top_genreLM'),
+    path('LightMode/listening_minutes/', views.listening_minutes_lm, name='listening_minutesLM'),
+    path('LightMode/type_of_listener/', views.type_of_listener_lm, name='type_of_listenerLM'),
+    path('LightMode/memorable_moment/', views.memorable_moment_lm, name='memorable_momentLM'),
 ]
--- navigation/views.py
+++ navigation/views.py
@@ -30,3 +30,33 @@ def type_of_listener_rb(request):
 
 def memorable_moment_rb(request):
     return render(request, 'RainbowMode/memorable_moment.html')
+
+def transition1_lm(request):
+    return render(request, 'LightMode/transition1.html')
+
+def transition2_lm(request):
+    return render(request, 'LightMode/transition2.html')
+
+def slideshow_lm(request):
+    return render(request, 'LightMode/slideShowLM.html')
+
+def top_5_songs_lm(request):
+    return render(request, 'LightMode/top_5_songs.html')
+
+def top_song_lm(request):
+    return render(request, 'LightMode/top_song.html')
+
+def top_artist_lm(request):
+  return render(request, 'LightMode/top_artists.html')
+
+def top_genre_lm(request):
+    return render(request, 'LightMode/top_genre.html')
+
+def listening_minutes_lm(request):
+    return render(request, 'LightMode/listening_minutes.html')
+
+def type_of_listener_lm(request):
+    return render(request, 'LightMode/type_of_listener.html')
+
+def memorable_moment_lm(request):
+    return render(request, 'LightMode/memorable_moment.html')
